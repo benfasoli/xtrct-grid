80ae5ab3daa6323a5e6a17a6e5b9df1a77b99c0c
Ben Fasoli <benfasoli@gmail.com>
2019-07-11 15:11:43 -0700

    Move parser to `__main__` block

--- entrypoint.py
+++ entrypoint.py
@@ -84,30 +84,29 @@ def xtrct_grid(input: str,
     os.rename(output, os.path.join(output_dir, output))
 
 
-parser = argparse.ArgumentParser(
-    description='Extract spatial subdomain from ARL packed file.')
-
-parser.add_argument('-l', '--xmin', type=longitude, required=True,
-                    help='minimum x coordinate of subdomain')
-parser.add_argument('-r', '--xmax', type=longitude, required=True,
-                    help='maximum x coordinate of subdomain')
-parser.add_argument('-b', '--ymin', type=latitude, required=True,
-                    help='minimum y coordinate of subdomain')
-parser.add_argument('-t', '--ymax', type=latitude, required=True,
-                    help='maximum y coordinate of subdomain')
-                    
-parser.add_argument('-i', '--input', type=str, required=True,
-                    help='input data file name without path')
-parser.add_argument('-o', '--output', type=str, required=True,
-                    help='output data file name')
-parser.add_argument('--input_dir', type=str, default='/input',
-                    help='mount location for input data volume')
-parser.add_argument('--output_dir', type=str, default='/output',
-                    help='mount location for output data volume')
-parser.add_argument('--levels', type=int, default=36,
-                    help='vertical levels to extract')
-
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Extract spatial subdomain from ARL packed file.')
+
+    parser.add_argument('-l', '--xmin', type=longitude, required=True,
+                        help='minimum x coordinate of subdomain')
+    parser.add_argument('-r', '--xmax', type=longitude, required=True,
+                        help='maximum x coordinate of subdomain')
+    parser.add_argument('-b', '--ymin', type=latitude, required=True,
+                        help='minimum y coordinate of subdomain')
+    parser.add_argument('-t', '--ymax', type=latitude, required=True,
+                        help='maximum y coordinate of subdomain')
+
+    parser.add_argument('-i', '--input', type=str, required=True,
+                        help='input data file name without path')
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='output data file name')
+    parser.add_argument('--input_dir', type=str, default='/input',
+                        help='mount location for input data volume')
+    parser.add_argument('--output_dir', type=str, default='/output',
+                        help='mount location for output data volume')
+    parser.add_argument('--levels', type=int, default=36,
+                        help='vertical levels to extract')
+                        
     args = parser.parse_args()
     xtrct_grid(**vars(args))
